eb954f0e565d922ac77b0ed78d065062a7ea1be3
Ali <ali.wong@polyu.edu.hk>
2021-11-29 10:55:09 +0800

    Added FormatFileSizeTrait.

--- src/Format.php
+++ src/Format.php
@@ -1,90 +1,9 @@
 <?php namespace Wongyip\PHPHelpers;
 
+use Wongyip\PHPHelpers\Traits\FormatFileSizeTrait;
+use Wongyip\PHPHelpers\Traits\FormatStringTrait;
+
 class Format
 {
-    /**
-     * Turn a string into camel-case style.
-     * e.g. "Quick brown-fox jumps over the lazy dog!" >> "quickBrownFoxJumpsOverTheLazyDog"
-     * 
-     * Note that the default pattern break input into parts with any non-alphanumeric chars, and all such chars will be removed from output.
-     * 
-     * @param string $string
-     * @param string $block_pattern
-     * @return string
-     */
-    static function camelCase($string, $block_pattern = "/[a-z0-9]+/i")
-    {
-        $string = trim($string);
-        $output = '';
-        if (preg_match_all($block_pattern, $string, $matches)) {
-            foreach ($matches[0] as $part){
-                $output .= ucfirst(strtolower(trim($part)));
-            }
-            return lcfirst($output);
-        }
-        return strtolower($string);
-    }
-    
-    /**
-     * Make a phrase into "Smart Capitalized Format for Titles and Headers".
-     * 
-     * @param string  $phrase        Input phrase.
-     * @param array   $allLower      List of words in all lower-case, default ['a', 'an', 'the', 'and', 'of', 'at', 'by', 'on', 'for', 'in', 'into', 'onto'].
-     * @param array   $allUpper      List of words in all upper-case, default ['ID'].
-     * @param array   $doNotTouch    List of words that shall keep untouched, no default.
-     * @param boolean $ignoreNonWord Skip words composed non-alphabet words, default true.
-     * @throws \Exception
-     * @return string
-     */
-    static function smartCaps($phrase, $allLower = null, $allUpper = null, $doNotTouch = null, $ignoreNonWord = true)
-    {
-        $allLower = is_array($allLower) ? $allLower : ['a', 'an', 'the', 'and', 'of', 'at', 'by', 'on', 'for', 'in', 'into', 'onto'];
-        $allUpper = is_array($allUpper) ? $allUpper : ['ID'];
-        if (is_string($phrase)) {
-            $parts = preg_split('/\-|_|\s/', $phrase);
-            foreach ($parts as $key => $part) {
-                $parts[$key] = in_array(strtolower($part), $allLower)
-                    ? strtolower($part)
-                    : (in_array(strtoupper($part), $allUpper)
-                        ? strtoupper($part)
-                        : (!self::isWord($part) && $ignoreNonWord
-                            ? $part
-                            : ucfirst($part)
-                        )
-                    );
-            }
-            return !self::isWord($part[0]) && $ignoreNonWord ? implode(' ', $parts) : ucfirst(implode(' ', $parts));
-        }
-        return '';
-    }
-    
-    /**
-     * @param string $input
-     * @return boolean
-     */
-    static function isWord($input)
-    {
-        if (preg_match("/^[a-z]*$/i", $input)) {
-            return true;
-        }
-        return false;
-    }
-    
-    /**
-     * String indentation.
-     * 
-     * @param string  $string    The input string
-     * @param integer $depth     Levels of indentation.
-     * @param integer $indentStr Indentation string, default 4 spaces.
-     * @return string
-     */
-    static function indent($string, $depth = 1, $indentStr = null)
-    {
-        if (!empty($string)) {
-            $indentStr = is_string($indentStr) ? $indentStr : str_repeat(' ', 4);
-            $indent = str_repeat($indentStr, $depth);
-            return preg_replace("/^/m", $indent, $string);
-        }
-        return $string;
-    }
+    use FormatFileSizeTrait, FormatStringTrait;
 }
---
+++ src/Traits/FormatFileSizeTrait.php
@@ -0,0 +1,57 @@
+<?php namespace Wongyip\PHPHelpers\Traits;
+
+trait FormatFileSizeTrait
+{
+    const WY_FILESIZE_UNITS_BINARY_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
+    const WY_FILESIZE_UNITS_BINARY_STRICT = 'KiB,MiB,GiB,TiB,PiB,EiB,ZiB,YiB';
+    const WY_FILESIZE_UNITS_METRIC_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
+    const WY_FILESIZE_UNITS_METRIC_STRICT = 'kB,MB,GB,TB,PB,EB,ZB,YB';
+    
+    /**
+     * Convert byte-size to human readable file size.
+     *
+     * Modified from: https://stackoverflow.com/a/14919494
+     * Reference: https://en.wikipedia.org/wiki/File_size
+     * Reference: https://physics.nist.gov/cuu/Units/prefixes.html
+     *
+     * @param number  $bytes   File size in bytes.
+     * @param boolean $prefix  Default to binary prefixes, insert any non-null value use metric prefixes (or SI prefixes, whiere kilo = 10^3).
+     * @param boolean $unit    Default to strict units, insert any non-null value for casual unit.
+     */
+    static function fileSize($bytes, $prefix = null, $unit = null, $precision = 2)
+    {
+        if (!is_numeric($bytes) && $bytes >= 0) {
+            throw new \Exception('Accept non-negative integer $bytes only, input: ' . $bytes);
+        }
+        
+        // Options
+        $binary = is_null($prefix);
+        $strict = is_null($unit);
+        $thresh = $binary ? 1024 : 1000;
+        
+        // No conversion is needed
+        if (abs($bytes) < $thresh) {
+            return $bytes . ' Bytes';
+        }
+        
+        // Pick the units.
+        $units = explode(
+            ',',
+            $binary
+                ? ($strict ? self::WY_FILESIZE_UNITS_BINARY_STRICT : self::WY_FILESIZE_UNITS_BINARY_CASUAL)
+                : ($strict ? self::WY_FILESIZE_UNITS_METRIC_STRICT : self::WY_FILESIZE_UNITS_METRIC_CASUAL)
+        );
+        
+        // Do the calculations
+        $u = -1;
+        $output = $bytes;
+        while (abs($output) >= $thresh & $u < count($units) - 1) {
+            $output /= $thresh;
+            $u++;
+        }
+        
+        // Yo
+        $output = round($output, $precision);
+        return $output . ' ' . $units[$u];        
+    }
+}
