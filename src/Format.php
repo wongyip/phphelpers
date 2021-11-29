0c6c976829d069d8be85c72a436ea076a81a5575
Ali <ali.wong@polyu.edu.hk>
2021-11-29 11:01:52 +0800

    Bug fixed.

--- src/Format.php
+++ src/Format.php
@@ -6,4 +6,11 @@ use Wongyip\PHPHelpers\Traits\FormatStringTrait;
 class Format
 {
     use FormatFileSizeTrait, FormatStringTrait;
+
+    // For FormatFileSizeTrait.
+    const WY_FILESIZE_UNITS_BINARY_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
+    const WY_FILESIZE_UNITS_BINARY_STRICT = 'KiB,MiB,GiB,TiB,PiB,EiB,ZiB,YiB';
+    const WY_FILESIZE_UNITS_METRIC_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
+    const WY_FILESIZE_UNITS_METRIC_STRICT = 'kB,MB,GB,TB,PB,EB,ZB,YB';
+    
 }
--- src/Traits/FormatFileSizeTrait.php
+++ src/Traits/FormatFileSizeTrait.php
@@ -2,11 +2,6 @@
 
 trait FormatFileSizeTrait
 {
-    const WY_FILESIZE_UNITS_BINARY_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
-    const WY_FILESIZE_UNITS_BINARY_STRICT = 'KiB,MiB,GiB,TiB,PiB,EiB,ZiB,YiB';
-    const WY_FILESIZE_UNITS_METRIC_CASUAL = 'KB,MB,GB,TB,PB,EB,ZB,YB';
-    const WY_FILESIZE_UNITS_METRIC_STRICT = 'kB,MB,GB,TB,PB,EB,ZB,YB';
-    
     /**
      * Convert byte-size to human readable file size.
      *
