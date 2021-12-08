7df3a3b467bae98c26c28d9d9653ced4046a1401
Ali <ali.wong@polyu.edu.hk>
2021-12-08 14:11:48 +0800

    Added CSS::class.

---
+++ src/CSS.php
@@ -0,0 +1,36 @@
+<?php namespace Wongyip\PHPHelpers;
+
+class CSS
+{
+    /**
+     * Compose a space sepatated CSS classes string, for the HTML 'class' attribute.
+     * 
+     * @param string|string[] $original
+     * @param string|string[] $add
+     * @param string|string[] $remove
+     * @return string
+     */
+    static function classAttribute($original, $add, $remove)
+    {
+        $classes = is_string($original) ? explode(' ', $original) : (is_array($original) ? $original : []); 
+        
+        // Add first
+        if (!empty($add)) {
+            $adds = is_array($add) ? $add : explode(' ', $add);
+        }
+        else {
+            $adds = [];
+        }
+        
+        // Then remove
+        if (!empty($remove)) {
+            $removes = is_array($remove) ? $remove : explode(' ', $remove);
+        }
+        else {
+            $removes = [];
+        }
+        
+        // Uniqie < Diff < Merge
+        return implode(' ', array_unique(array_diff(array_merge($classes, $adds), $removes)));
+    }
+}
